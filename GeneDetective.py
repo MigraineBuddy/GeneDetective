0ae7becad40f772f63f269c6f96afeb28c2ae677
Ahmet Levent Zengin <applemacintosh420@gmail.com>
2023-10-26 22:31:06 +0300

    Dead inside. Also added instructions.

--- GeneDetective.py
+++ GeneDetective.py
@@ -27,12 +27,14 @@ warranty obviously not included.
 """
 
 
-import sys, argparse
+import argparse
+
 
 def write_dict_to_file(dictionary, output_file):
     with open(output_file, "w") as file:
         for key, value in dictionary.items():
             file.write(f"{key} {value}\n")
+
 
 def change_dict_key(dict, old_key, new_key, default_value=None):
     dict[new_key] = dict.pop(old_key, default_value)
@@ -45,13 +47,15 @@ def extract_to_dictionary(file_path):
             for line in file:
                 if len(line) >= 11:
                     key = line[:10]
-                    value = line[11:].strip()  # Remove leading and trailing whitespaces
+                    # Remove leading and trailing whitespaces
+                    value = line[11:].strip()
                     result[key] = value
                 else:
                     print(f"Skipped line: {line} (insufficient length)")
     except FileNotFoundError:
         print("File not found.")
     return result
+
 
 def compare_and_replace_dicts(dict1, dict2):
     result_dict = dict1.copy()  # Create a copy of the first dictionary
@@ -60,12 +64,14 @@ def compare_and_replace_dicts(dict1, dict2):
         if key in result_dict:
             result_dict[key] = value  # Replace the definition
             new_key = "[x] " + key
-            change_dict_key(result_dict, key, new_key) # Marks changed key
+            change_dict_key(result_dict, key, new_key)  # Marks changed key
 
     return result_dict
 
+
 def main():
-    parser = argparse.ArgumentParser(description="Compare and replace dictionaries")
+    parser = argparse.ArgumentParser(
+        description="GeneDetective requires a primary and secondary file to function, it'll find matching entries on both and replace the values of the matching entries on the secondary with the primary file and output the result to a text files with a desired user path. Changes are also marked with [x].")
     parser.add_argument("file1", help="Path to the first file")
     parser.add_argument("file2", help="Path to the second file")
     parser.add_argument("output", help="Path to the output file")
@@ -74,11 +80,9 @@ def main():
     dict1 = extract_to_dictionary(args.file1)
     dict2 = extract_to_dictionary(args.file2)
     output = compare_and_replace_dicts(dict1, dict2)
-    
+
     write_dict_to_file(output, args.output)
 
 
-
 if __name__ == "__main__":
-   main()
-
+    main()
